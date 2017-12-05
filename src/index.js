04b4628f01789ca59d52ea699655f2c225c4d12e
John Anthony Rivera <coffeeexistence@users.noreply.github.com>
2017-12-04 23:51:28 -0800

    Clean up callAsyncTask

--- src/index.js
+++ src/index.js
@@ -1,45 +1,53 @@
 import { Observable } from 'rxjs';
 
-export const callAsyncTask = asyncTask => source =>
-  Observable.create(observer => {
-    const put = actionToEmit => observer.next(actionToEmit);
+const callAsyncTask = (asyncTask, source, observer, action) => {
+  const put = actionToEmit => observer.next(actionToEmit);
 
-    const take = filterFn =>
-      new Promise(resolve =>
-        source
-          .filter(filterFn)
-          .map(resolve)
-          .take(1)
-          .subscribe(),
+  const take = filterFn =>
+    new Promise(resolve =>
+      source
+        .filter(filterFn)
+        .map(resolve)
+        .take(1)
+        .subscribe(),
+    );
+
+  const call = (childAsyncTask, ...taskArgs) => {
+    try {
+      return childAsyncTask({ put, call, take }, ...taskArgs);
+    } catch (error) {
+      console.error('Issue with asyncTask', error);
+      throw error;
+    }
+  };
+
+  const finished = () => {
+    try {
+      observer.complete();
+    } catch (error) {
+      // Without this, errors in the subscriber's onComplete handler can be
+      // swallowed silently, which makes for tricky debugging
+      console.error('Error in subscriber onComplete handler', error);
+      throw error;
+    }
+  };
+
+  asyncTask({ put, call, take })
+    .then(finished)
+    .catch(error => {
+      console.error(
+        `asyncTask rejected due to following error: ${error.message}`,
+        error,
       );
+      observer.error(error);
+    });
+};
 
-    const call = (childAsyncTask, ...taskArgs) => {
-      try {
-        return childAsyncTask({ put, call, take }, ...taskArgs);
-      } catch (error) {
-        console.error('Issue with asyncTask', error);
-        throw error;
-      }
-    };
-
-    const finished = () => {
-      try {
-        observer.complete();
-      } catch (error) {
-        // Without this, errors in the subscriber's onComplete handler can be
-        // swallowed silently, which makes for tricky debugging
-        console.error('Error in subscriber onComplete handler', error);
-        throw error;
-      }
-    };
-
-    asyncTask({ put, call, take })
-      .then(finished)
-      .catch(error => {
-        console.error(
-          `asyncTask rejected due to following error: ${error.message}`,
-          error,
-        );
-        observer.error(error);
-      });
-  });
+export default asyncTask => {
+  const source = this;
+  return source.mergeMap(action =>
+    Observable.create(observer =>
+      callAsyncTask(asyncTask, source, observer, action),
+    ),
+  );
+};
